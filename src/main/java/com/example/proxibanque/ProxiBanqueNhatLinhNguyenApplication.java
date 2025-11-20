10f19cca4741cefbe808da322206117ec9f86bb4
nlnguyen <linhbaljeet0208@gmail.com>
2025-11-20 10:32:07 +0100

    First commit

---
+++ src/main/java/com/example/proxibanque/ProxiBanqueNhatLinhNguyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.proxibanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiBanqueNhatLinhNguyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxiBanqueNhatLinhNguyenApplication.class, args);
+    }
+
+}
